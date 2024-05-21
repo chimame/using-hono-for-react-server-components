23e4a66b769b52c276d34fe23fb77a550b2314ba
chimame <rito.tamata@gmail.com>
2024-05-22 02:00:01 +0900

    first commit

---
+++ exercises/02.server-components/server/app.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { serve, type HttpBindings } from '@hono/node-server'
+import { RESPONSE_ALREADY_SENT } from '@hono/node-server/utils/response'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
+import { trimTrailingSlash } from 'hono/trailing-slash'
+import { renderToPipeableStream } from 'react-server-dom-esm/server'
+import { App } from './components/App'
+
+const PORT = 3000
+
+const app = new Hono<{ Bindings: HttpBindings }>({ strict: true })
+app.use(trimTrailingSlash())
+app.use(logger())
+app.use(cors({
+	origin: 'http://localhost:5173',
+	allowMethods: ['POST', 'GET', 'OPTIONS'],
+	exposeHeaders: ['Content-Length'],
+	maxAge: 600,
+	credentials: true,
+}))
+
+app.get('/rsc/:dataId?', async context => {
+  const { pipe } = renderToPipeableStream(<App />)
+	// MEMO: Configure CROS to return with `RESPONSE ALREADY SENT`
+	context.env.outgoing.setHeader('Content-Type', 'text/html')
+	context.env.outgoing.setHeader('Access-Control-Allow-Credentials', 'true')
+	context.env.outgoing.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+	context.env.outgoing.setHeader('Access-Control-Expose-Headers', 'Content-Length')
+
+  pipe(context.env.outgoing)
+  return RESPONSE_ALREADY_SENT
+})
+
+app.onError((err, context) => {
+  console.error('error', err)
+  return context.json({ error: true, message: 'Something went wrong' }, 500)
+})
+
+serve({ fetch: app.fetch, port: PORT }, info => {
+  const url = `http://localhost:${info.port}`
+  console.log(`🚀  We have liftoff!\n${url}`)
+})
---
+++ exercises/02.server-components/server/components/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
